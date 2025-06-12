4baaaf682a46512ec79e9db3e526152cf90ed99e
zachkr05 <zachkr05@gmail.com>
2025-06-12 23:33:33 +0000

    debug

--- include/mpcc/arutils.h
+++ include/mpcc/arutils.h
@@ -6,8 +6,23 @@
 #include <string>
 namespace PathPlanning{
 	struct Goal{
-		double position;
+		Eigen::Vector2d position;
 		double k_att;
+
+
+		inline void setPosition(Eigen::Vector2d& newPosition) { position = newPosition}
+		inline void setAttractiveGain(double newForce) {k_att = newForce}
+
+		inline const double getAttractiveGain() {return k_att}
+		inline const Eigen::Vector2d getPosition() {return position}
+
+		inline const Eigen::Vector2d getAttractiveForce(Eigen::Vector2d point){
+			return 0.5*k_att * (point-position).squared.Norm()
+		}
+		inline const Eigen::Vector2d getAttractiveGradient(Eigen::Vector2d point){
+			return -k_att * (point * goal);
+		}
+
 		};
 	
 	struct Obstacle {
@@ -19,7 +34,7 @@ namespace PathPlanning{
 
 		//Setters
 		
-		inline void setPosition(Eigen::Vector2d newPosition){
+		inline void setPosition(Eigen::Vector2d& newPosition){
 			position = newPosition;
 		}
 
@@ -38,21 +53,21 @@ namespace PathPlanning{
 			return sigma
 		}
 
-		inline const Eigen::Vector2d getPosition() const noexcept{
+		inline const Eigen::Vector2d& getPosition() const noexcept{
 			return position
 		}
 		
-		inline double getPotential(Eigen::Vector2d point){
-			Eigen::Vector2d diffs = position - point
+		inline double getPotential(Eigen::Vector2d& point){
+			Eigen::Vector2d diffs = point - position
 			double ssd = diff.squaredNorm();
 			double coeff = std::exp(-ssd / (2.0 * sigma * sigma))
 			return amplitude * coeff
 		}
 		inline Eigen::Vector2d getGradient(Eigen::Vector2d point){
-			Eigen::Vector2d diffs = position - point
+			Eigen::Vector2d diffs = point - position
 			double ssd = diffs.dot(diffs)
 			double coeff = std::exp(-ssd / (2.0 * sigma * sigma))
-			return amplitude * coeff * diffs / (sigma**2)
+			return amplitude * coeff * diffs / (sigma*sigma)
 		}
 		
 	};
